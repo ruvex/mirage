f64f713db75be736c408b2397cfff76e416548ea
x3coin <x3coin@gmail.com>
2018-10-27 12:44:18 +0200

    Changed DNS seed

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -104,8 +104,7 @@ public:
 		base58Prefixes[EXT_PUBLIC_KEY] = list_of(0x04)(0x88)(0xB2)(0x12).convert_to_container<std::vector<unsigned char> >();
 		base58Prefixes[EXT_SECRET_KEY] = list_of(0x04)(0x88)(0xAD)(0xE2).convert_to_container<std::vector<unsigned char> >();
 
-		vSeeds.push_back(CDNSSeedData("0", "seed1.x3c.io"));
-		vSeeds.push_back(CDNSSeedData("1", "seed2.x3c.io"));
+		vSeeds.push_back(CDNSSeedData("0", "seeder.x3c.io"));
 		convertSeeds(vFixedSeeds, pnSeed, ARRAYLEN(pnSeed), nDefaultPort);
 
 		nPoolMaxTransactions = 3;
